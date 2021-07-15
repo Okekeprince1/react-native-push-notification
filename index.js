7796d0718d26041c41072d699d930977416ce928
Okekeprince1 <okeke.p.ebube@gmail.com>
2021-07-15 20:48:31 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+import { AppRegistry } from 'react-native';
+import { name as appName } from './app.json';
+// eslint-disable-next-line import/extensions
+import App from './App';
+
+AppRegistry.registerComponent(appName, () => App);
